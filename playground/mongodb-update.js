ec93a1642cf6ee46f2a7e86240cc090484c34d8a
Ashish <ashish.raist@gmail.com>
2018-07-27 18:36:06 -0400

    update added

--- playground/mongodb-update.js
+++ playground/mongodb-update.js
@@ -5,6 +5,20 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp',(err,db)=>{
         return console.log('Connection could not be established',err);
     }
    
+    db.collection('user').findOneAndUpdate({
+        _id : new ObjectID('5b5b82aabaeb2811f8151ce5')},
+   
+    {
+        $inc:{
+            'Age':+1
+        }
+    },
+    {
+        returnOriginal: false
+    }
+).then((result)=>{
+        console.log(result);
+    });
 
     // db.close();
 });
