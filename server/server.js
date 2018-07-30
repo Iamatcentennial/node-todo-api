c0ae550327f4c02384b52d17beea4755782ebed1
Ashish <ashish.raist@gmail.com>
2018-07-30 18:41:33 -0400

    GET and POST test on postman

--- server/server.js
+++ server/server.js
@@ -36,8 +36,19 @@ app.post('/users',(req,res)=>{
     });
 });
 
+app.get('/todos',(req,res)=>{
+   Todo.find().then((todos)=>{
+       res.send({
+           todos
+       });
+   }, (e)=>{
+       res.status.send(e);
+   }); 
+});
 
 app.listen(3000, ()=>{
     console.log('Started on port 3000');
 });
+
+
 module.exports = { app }
