a7d35407294fcbbe496393c938c67fca90342c72
Ashish <ashish.raist@gmail.com>
2018-08-01 01:24:34 -0400

    PATCH/todos/:id added

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 //Library imports
-var express = require('express');
-var bodyparser = require('body-parser');
+const express = require('express');
+const bodyparser = require('body-parser');
 const {ObjectID} = require('mongodb');
+const _ = require('lodash');
 
 
 //Local imports
@@ -90,7 +91,33 @@ app.delete('/todos/:id',(req, res)=>{
 
 });
 
+app.patch('/todos/:id',(req,res)=>{
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text' ,'completed']);
 
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+    
+    if(_.isBoolean(body.completed) && body.completed ){
+        body.completedAt = new Date().getTime();
+    }
+    else{
+        body.completed=false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id,{$set:body}, {new: true})
+    .then((todo)=>{
+        if(!todo)
+        return res.status(404).send();
+
+        res.send({ todo});
+    }).catch((e)=>{
+        return res.status(404).send();
+    });
+
+});
 
 
 
--- tests/server.test.js
+++ tests/server.test.js
@@ -2,6 +2,7 @@ const expect = require('expect');
 const request = require('supertest');
 const { ObjectID } = require('mongodb');
 
+
 const { app } = require('./../server/server');
 const { Todo } = require('./../server/models/todo');
 
